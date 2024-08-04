d402b52cfa01ccd62c2862999832ebac997022b5
Yashwanth <yaswanthreddy909@gmail.com>
2024-08-04 21:50:06 +0530

    format: removed the unused function helpers functions

--- utils/helpers.go
+++ utils/helpers.go
@@ -1,30 +1,3 @@
 package utils
 
-import (
-	"encoding/json"
-)
-
-func JsonSerilizeDeserialize(src any, dest any) error {
-	data, err := json.Marshal(src)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(data, dest)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func GetBytes(d any) ([]byte, error) {
-	return json.Marshal(d)
-	// var buf bytes.Buffer
-	// enc := gob.NewEncoder(&buf)
-	// err := enc.Encode(d)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// return buf.Bytes(), nil
-}
+func Add(a, b int) int { return a + b }
