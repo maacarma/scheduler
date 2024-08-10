29e02e664765bf0ddda708048d3d42142335261f
Prudhvi0717 <reachme.pruthvi@gmail.com>
2024-08-11 04:18:02 +0530

    feat: added logger and graceful shutdown

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -2,31 +2,54 @@ package api
 
 import (
 	"context"
-	"log"
+	"net/http"
+	"time"
 
-	postgres "github.com/maacarma/scheduler/pkg/db/postgres"
-	scheduler "github.com/maacarma/scheduler/pkg/services/scheduler/transport"
+	db "github.com/maacarma/scheduler/pkg/db"
+	tasks "github.com/maacarma/scheduler/pkg/services/tasks/transport"
+	utils "github.com/maacarma/scheduler/utils"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
-func Start() {
+// Start starts the API server
+func Start(ctx context.Context, logger *zap.Logger, conf *utils.Config) error {
 
-	pgConn, err := postgres.Connect(context.Background())
+	pgConn, mongoClient, err := db.Connect(ctx, conf)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	defer func() {
 		if pgConn != nil {
-			pgConn.Close(context.Background())
+			pgConn.Close(ctx)
+		}
+		if mongoClient != nil {
+			mongoClient.Disconnect(ctx)
 		}
 	}()
 
 	r := gin.Default()
-	scheduler.Activate(r, pgConn)
+	tasks.Activate(r, pgConn, mongoClient)
 
-	if err := r.Run(); err != nil {
-		log.Fatal(err)
+	errch := make(chan error)
+	server := &http.Server{
+		Addr:    conf.Application.Port,
+		Handler: r,
+	}
+
+	go func() {
+		logger.Info("Starting server", zap.String("addr", server.Addr))
+		errch <- server.ListenAndServe()
+	}()
+
+	select {
+	case err := <-errch:
+		return err
+	case <-ctx.Done():
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		return server.Shutdown(shutdownCtx)
 	}
 }
