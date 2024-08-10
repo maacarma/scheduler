4dc53b0fd896f46e0406e66f30513243b5a0e830
Prudhvi0717 <reachme.pruthvi@gmail.com>
2024-08-11 04:15:06 +0530

    chore: changed service name from scheduler to tasks

--- pkg/services/scheduler/store/postgres/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (
-  id         BIGSERIAL PRIMARY KEY,
-  url        text      NOT NULL,
-  method     text      NOT NULL,
-  namespace  text      NOT NULL,
-  params     json,
-  headers    json,
-  body       json
-);
